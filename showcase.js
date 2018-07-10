562e8ddcacbcac510548dbb17e2470a27b65e31c
ElectroPanda01 <electricpanda01@gmail.com>
2018-07-10 15:22:22 -0700

    Showcase update

--- showcase.js
+++ showcase.js
@@ -5,9 +5,9 @@ module.exports = function (msg) {
   function correctFormat (msg) {
     msg.author.send(`**Your Message**\n\`\`\`\n${msg.content}\n\`\`\``)
     msg.delete()
-    msg.author.send('**Showcase Rules**\n\n- We only accept ``.png`` and ``.jpg`` files!\n- If you are responding to someones showcase, do it in the corresponding channel.\n*If you have any questions, contact <244279642092077056> or [Panda]#3431*')
+    msg.author.send('**Showcase Rules**\n\n- We only accept ``.png``, ``.jpg``, ``Illustrator (.ai)``, ``Photoshop (.psd)``, ``Gif (.gif)``, ``Vector (.svg)`` files!\n- If you are responding to someones showcase, do it in the corresponding channel.\n If there is a file that we do not currently support, please direct message Panda (details below) with the file you are wanting to add.\n*If you have any questions, contact <244279642092077056> or [Panda]#3431*')
   }
-  if (files.length && /\.(pn|jpe?)g$/.test(files[0].filename)) {
+  if (files.length && /\.(ai|psd|gif|svg|png|jpe?g)$/.test(files[0].filename)) {
   } else {
     correctFormat(msg)
   }
