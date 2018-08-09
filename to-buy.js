b763eb43d4d078f9eb9b85b77c1f3fb9e8f9c4e7
ElectroPanda01 <electricpanda01@gmail.com>
2018-08-08 17:08:31 -0700

    to-buy test

--- to-buy.js
+++ to-buy.js
@@ -3,7 +3,7 @@ module.exports = function (msg) {
   if (msg.member.hasPermission('MANAGE_MESSAGES')) return
 
   function correctFormat (msg) {
-    msg.author.send(`**Your Message**\n\`\`\`\n${msg.content}\n\`\`\``)
+    msg.author.send(`\n\`\`\`\n \n\`\`\`\n**Your Message**\n\`\`\`\n${msg.content}\n\`\`\``)
     msg.delete()
     msg.author.send('**Buy Format**\n\nTo ensure organization and everyone getting a fair chance to find an artist, please follow this format:\n```Name: \nPrice Range: \nDetails: \nContact Info: ```\n__Please Note:__ "$0" or "Free" does not mean that you are buying anything. Your message will be deleted if you are using those "price ranges"\n\n*If you have any questions, contact <244279642092077056> or [Panda]#3431*')
   }
