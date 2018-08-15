a08daac51599ea31336dcc26b2f852359d5df1e0
ElectroPanda01 <electricpanda01@gmail.com>
2018-08-15 15:46:44 -0700

    More test

--- commands/haveyoudonethis.js
+++ commands/haveyoudonethis.js
@@ -1,8 +1,10 @@
 const Discord = require('discord.js')
 
 module.exports.run = async (bot, message, args) => {
+  if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("You do not have permission to do that.");
+  if(!args[0]) return message.channel.send("no");
+
   let ughembed = new Discord.RichEmbed()
-  .setAuthor(message.author.tag)
   .setColor('#42f471')
   .addField("Okay, let me tell you something.",
     "Staff here is very busy and cannot deal with those who cannot be bothered to read the various channels we have set up to ensure that people have tools and knowledge to navigate discord and this server in particular. Please read #faq, #guidelines, #rules, and #announcements BEFORE proceeding.");
